95faf162eee7aaed1bee32be21d8fb8375e69e9b
Thorsten Sommer <mail@tsommer.org>
2024-10-20 18:55:10 +0200

    Defined transformer to add meta data to the document

--- DemoServer/Program.cs
+++ DemoServer/Program.cs
@@ -3,7 +3,27 @@ using System.Text.Json.Serialization;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "EDI - (E)xternal (D)ata AP(I) for AI Studio",
+            Version = "v1",
+            Description = """
+                          This API serves as a contract between AI Studio and any external data sources. AI Studio
+                          acts as the client and the data sources act as the server. The data sources implement some
+                          form of data retrieval and return a suitable context to AI Studio. AI Studio, in turn,
+                          handles the integration of appropriate LLMs. Data sources can be document or graph databases
+                          or file systems, for example. They will likely implement an appropriate RAG process. However,
+                          this API does not inherently require RAG, as data processing is implemented decentralized by
+                          the data sources.
+                          """
+        };
+        return Task.CompletedTask;
+    });
+});
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
