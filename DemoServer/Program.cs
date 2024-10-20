b97a73e84a47a6ba65b7de8251396b6d16174ee1
Thorsten Sommer <mail@tsommer.org>
2024-10-20 18:40:21 +0200

    Move example data to its own class

--- DemoServer/Program.cs
+++ DemoServer/Program.cs
@@ -18,17 +18,6 @@ app.UseSwaggerUi(settings =>
     settings.DocumentTitle = "(E)xternal (D)ata AP(I) for AI Studio";
 });
 
-// This is a simple demonstration for a data source. In fact, you would want to
-// connect a graph database or a document database or a server's file system instead:
-var exampleData = new[]
-{
-    new WikipediaArticle("Strategic foresight", "https://en.wikipedia.org/wiki/Strategic_foresight"),
-    new WikipediaArticle("Scenario planning", "https://en.wikipedia.org/wiki/Scenario_planning"),
-    new WikipediaArticle("Futures studies", "https://en.wikipedia.org/wiki/Futures_studies"),
-    new WikipediaArticle("Futures techniques", "https://en.wikipedia.org/wiki/Futures_techniques"),
-    new WikipediaArticle("Delphi method", "https://en.wikipedia.org/wiki/Delphi_method"),       
-};
-
 app.MapGet("/auth/methods", () => new List<AuthMethods> { AuthMethods.NONE })
     .WithDescription("Get the available authentication methods.")
     .WithTags("Authentication");
