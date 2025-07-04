c0a90aad7bed18d9d9a8347a938d08aa56e96852
Thorsten Sommer <SommerEngineering@users.noreply.github.com>
2025-07-04 10:12:14 +0200

    Refactored code base (#10)

---
+++ DemoServer/Handler/DataSource.cs
@@ -0,0 +1,18 @@
+﻿using DemoServer.DataModel;
+
+namespace DemoServer.Handler;
+
+public static class DataSource
+{
+    private static readonly DataSourceInfo DATA_SOURCE_INFO = new("DEMO: Wikipedia Links", "Contains some links to Wikipedia articles.");
+
+    public static void AddDataSourceHandlers(this WebApplication app)
+    {
+        app.MapGet("/dataSource", GetDataSourceInfo)
+            .WithDescription("Get information about the data source.")
+            .WithName("GetDataSourceInfo")
+            .WithTags("Data Source");
+    }
+
+    private static DataSourceInfo GetDataSourceInfo() => DATA_SOURCE_INFO;
+}
---
+++ DemoServer/Handler/Embedding.cs
@@ -0,0 +1,35 @@
+﻿using DemoServer.DataModel;
+
+namespace DemoServer.Handler;
+
+public static class Embedding
+{
+    private static readonly EmbeddingInfo[] EMBEDDING_INFO =
+    [
+        //
+        // Just to demonstrate the usage of the EmbeddingInfo record.
+        // This demo server uses doesn't use any embedding, though.
+        //
+        // In case a data source uses no embedding, it can return an
+        // empty list.
+        //
+        new()
+        {
+            EmbeddingType = "Transformer Embedding",
+            EmbeddingName = "OpenAI: text-embedding-3-large",
+            Description = "Uses the text-embedding-3-large model from OpenAI",
+            UsedWhen = "Anytime",
+            Link = "https://platform.openai.com/docs/guides/embeddings/embedding-models",
+        },
+    ];
+
+    public static void AddEmbeddingHandlers(this WebApplication app)
+    {
+        app.MapGet("/embedding/info", GetEmbeddingInfo)
+            .WithDescription("Get information about the used embedding(s).")
+            .WithName("GetEmbeddingInfo")
+            .WithTags("Embedding");
+    }
+
+    private static IEnumerable<EmbeddingInfo> GetEmbeddingInfo() => EMBEDDING_INFO;
+}
